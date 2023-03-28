7cd8ff234875421f435c736adbf030688caaf577
Rafael Rosa <rafaelcruzrosa1@gmail.com>
2023-03-28 18:53:42 -0300

    Participant factory

---
+++ src/domain/room/factory/participant.factory.ts
@@ -0,0 +1,11 @@
+import { randomUUID } from "crypto";
+import Participant from "../entities/participants";
+
+export default class ParticipantFactory {
+  static create(name: string) {
+    return new Participant({
+      name,
+      id: randomUUID()
+    })
+  }
+}
