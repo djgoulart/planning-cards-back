f26082e579d148e45779e1d37f826d4732b4ffa6
Rafael Rosa <rafaelcruzrosa1@gmail.com>
2023-03-28 18:53:24 -0300

    Join room method in interface

--- src/domain/room/repository/room.repository.ts
+++ src/domain/room/repository/room.repository.ts
@@ -1,5 +1,7 @@
+import Participant from "../entities/participants";
 import Room from "../entities/room";
 
 export interface RoomRepositoryInterface {
   create(data: Room): Promise<Room>;
+  joinRoom(participant: Participant, roomId: string): Promise<Room>
 }
