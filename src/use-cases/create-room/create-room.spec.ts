58047d4115823cbaed69befd22ee2980b26f0780
Rafael Rosa <rafaelcruzrosa1@gmail.com>
2023-03-28 18:56:23 -0300

    Changes

--- src/use-cases/create-room/create-room.spec.ts
+++ src/use-cases/create-room/create-room.spec.ts
@@ -1,24 +1,22 @@
-import RoomFactory from "@/domain/room/factory/room.factory";
+import RoomRepository from "../../infrastructure/prisma/repositories/room/room.repository";
+import { prismaMock } from "../../infrastructure/prisma/singleton"
+import  { validRoom } from "../../tests/mocks/repository/room-repository.mock";
 import { CreateRoomInputDto } from "./create-room.dto";
 import CreateRoomUseCase from "./create-room.use-case";
 
-const roomToCreate = RoomFactory.createEmptyRoom("Empty room");
-
-const roomRepository = {
-  create: () => {
-    return Promise.resolve(roomToCreate);
-  },
-};
 describe("Create Room use case test", () => {
   it("Should be able to create an room", async () => {
-    const useCase = new CreateRoomUseCase(roomRepository);
 
+    const useCase = new CreateRoomUseCase(new RoomRepository());
+    
     const input: CreateRoomInputDto = {
-      room: roomToCreate,
+      room: validRoom,
     };
+
+    prismaMock.room.update.mockResolvedValue(validRoom)
 
     const output = await useCase.execute(input);
 
-    expect(output.room.id).toEqual(roomToCreate.id);
+    expect(output.room.id).toEqual(validRoom.id);
   });
 });
